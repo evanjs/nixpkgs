bb39ea1c588f6a761ea943b4b957309c74380f52
Evan Stoll <evanjsx@gmail.com>
2020-02-16 13:21:56 -0500

    rtl8188eu: use driver from TP-Link

--- pkgs/os-specific/linux/rtl8188eu/default.nix
+++ pkgs/os-specific/linux/rtl8188eu/default.nix
@@ -1,30 +1,36 @@
-{ stdenv, lib, fetchFromGitHub, kernel }:
+{ stdenv, lib, fetchzip, kernel, unzip, bc }:
 
 with lib;
 
-let modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtlwifi/rtl8188eu";
+let modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtl8188eu";
 
 in stdenv.mkDerivation rec {
   name = "rtl8188eu-${kernel.version}-${version}";
-  version = "4.1.8_9499";
+  version = "2018-05-21-unstable";
 
-  src = fetchFromGitHub {
-    owner = "lwfinger";
-    repo = "rtl8188eu";
-    rev = "v${version}";
-    sha256 = "1bxpd1kan37zsdfwj9fb7fcpzxwyzynxqkcmcp3sflkq5gbgcgg5";
+  src = fetchzip {
+    url = https://static.tp-link.com/2018/201805/20180521/TP-Link_Driver_Linux_series7_beta.zip;
+    sha256 = "1r2bf08cjmldpsxdir42jndrifjzarmq1fp2cnzd9ja6r6s27rxr";
+    stripRoot = false;
   };
 
   hardeningDisable = [ "pic" ];
 
+  buildInputs = [ unzip bc ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
   makeFlags = [ "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" ];
 
   enableParallelBuilding = true;
 
+  postUnpack = ''
+    unzip $src/rtl8188EUS_linux_v5.2.2.4_25483.20171222.zip
+    mv rtl8188EUS_linux_v5.2.2.4_25483.20171222/* source/
+  '';
+
   postPatch = ''
-    substituteInPlace Makefile --replace 'all: test modules' 'all: modules'
+    substituteInPlace core/rtw_debug.c \
+      --replace 'RTW_PRINT_SEL(sel, "build time: %s %s\n", __DATE__, __TIME__)' ""
   '';
 
   installPhase = ''
